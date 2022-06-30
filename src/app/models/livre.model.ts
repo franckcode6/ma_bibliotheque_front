8fa1a43c64a5016b3c79f983dd89a04dc39ab6b0
Franck Quasevi <franckcode6@gmail.com>
2022-06-30 10:30:55 +0200

    livre model

---
+++ src/app/models/livre.model.ts
@@ -0,0 +1,11 @@
+export interface Livre {
+  id: any;
+  titre: string;
+  isbn: string;
+  dateDeParution: string;
+  type?: any;
+  editeur?: any;
+  auteur?: any;
+  categorie?: any;
+  utilisateur?: any;
+}
