76be27305b0028331f8070cd487f2058a4fdef4e
Franck Quasevi <franckcode6@gmail.com>
2022-07-01 16:10:57 +0200

    Mise en place du formulaire de connexion

--- src/app/views/utilisateur/connexion/connexion.component.ts
+++ src/app/views/utilisateur/connexion/connexion.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UtilisateurHttpService } from 'src/app/services/http/utilisateur-http.service';
 
 @Component({
   selector: 'app-connexion',
   templateUrl: './connexion.component.html',
-  styleUrls: ['./connexion.component.css']
+  styleUrls: ['./connexion.component.css'],
 })
 export class ConnexionComponent implements OnInit {
+  myForm!: FormGroup;
+  submitted: boolean = false;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private us: UtilisateurHttpService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.myForm = this.fb.group({
+      email: ['', Validators.required],
+      motDePasse: ['', Validators.required],
+    });
   }
 
+  submitForm(): void {
+    this.submitted = true;
+    if (this.myForm.valid) {
+      console.log('VALID FORM');
+    } else {
+      console.error('INVALID FORM');
+    }
+  }
 }
